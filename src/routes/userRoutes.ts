421bc3792a3553043a8599cdeb8b3839dc7161a2
Juliet Onyekaoha <happyjuliet@rocketmail.com>
2024-11-15 10:23:20 +0100

    created routes constant file

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { registerUser, loginUser } from './../controllers/userController';
+import { ROUTES } from './../constants/routes';
 
 const userRoutes = express();
 
-userRoutes.post('/register', (req, res) => {
+userRoutes.post(ROUTES.REGISTER, (req, res) => {
   registerUser(req, res);
 });
-userRoutes.post('/login', (req, res) => {
+userRoutes.post(ROUTES.LOGIN, (req, res) => {
   loginUser(req, res);
 });
 
