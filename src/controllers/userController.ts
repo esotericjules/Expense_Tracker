635a57090d424e32c67d465645a1578ad16d7c0d
Juliet Onyekaoha <happyjuliet@rocketmail.com>
2024-12-27 17:31:44 +0100

    seperated db logic to userModel file

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,15 +2,16 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { pool } from '../database/config/db';
 import { User } from '../types/user';
-import QUERIES from '../queries';
 import { handleRequestErrorResponse } from '../helpers';
+import { findUserByEmail, createUser } from '../models/userModel';
 dotenv.config();
 
 const secretKey = process.env.JWT_SECRET_KEY;
-export const registerUser = async (req: Request, res: Response): Promise<Response> => {
-  // Implement registration logic
+export const registerUser = async (
+  req: Request,
+  res: Response,
+): Promise<Response> => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
@@ -23,10 +24,9 @@ export const registerUser = async (req: Request, res: Response): Promise<Respons
 
   try {
     // check if user already exists
-    const findUserByEmailQuery = QUERIES.FIND_USER_BY_EMAIL;
-    const result = await pool.query(findUserByEmailQuery, [email]);
+    const existingUser: User = await findUserByEmail(email);
 
-    if (result.rows.length > 0) {
+    if (existingUser) {
       return handleRequestErrorResponse(res, 409, 'User already exists');
     }
 
@@ -34,19 +34,12 @@ export const registerUser = async (req: Request, res: Response): Promise<Respons
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // save user to database
-    const createUserQuery = QUERIES.CREATE_USER;
-    const newUser = await pool.query(createUserQuery, [
-      username,
-      email,
-      hashedPassword,
-    ]);
-
-    const user: User = newUser.rows[0];
+    const newUser: User = await createUser(username, email, hashedPassword);
 
     return res.status(201).json({
       message: 'User registered successfully',
       data: {
-        ...user,
+        ...newUser,
       },
     });
   } catch (error) {
@@ -55,7 +48,10 @@ export const registerUser = async (req: Request, res: Response): Promise<Respons
   }
 };
 
-export const loginUser = async (req: Request, res: Response): Promise<Response> => {
+export const loginUser = async (
+  req: Request,
+  res: Response,
+): Promise<Response> => {
   const { email, password } = req.body;
 
   if (!secretKey) {
@@ -72,14 +68,11 @@ export const loginUser = async (req: Request, res: Response): Promise<Response> 
   }
 
   try {
-    const findUserByEmailQuery = QUERIES.FIND_USER_BY_EMAIL;
-    const result = await pool.query(findUserByEmailQuery, [email]);
+    const user: User = await findUserByEmail(email);
 
-    if (result.rows.length === 0) {
+    if (!user) {
       return handleRequestErrorResponse(res, 404, 'User not found');
     }
-
-    const user: User = result.rows[0];
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
