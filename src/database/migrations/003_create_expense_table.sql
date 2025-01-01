99fba35b912d87ee6425f54df795fb82d1666041
Juliet Onyekaoha <happyjuliet@rocketmail.com>
2025-01-01 16:37:26 +0100

    added a non-negative constraint to expense amount

--- src/database/migrations/003_create_expense_table.sql
+++ src/database/migrations/003_create_expense_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE expense (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
-    amount DECIMAL(10, 2) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL CHECK (amount >= 0),
     description TEXT,
     category_id UUID NOT NULL REFERENCES category(id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
