cabff7b73c4ffda6f57081c2725b211e675f5272
Juliet Onyekaoha <happyjuliet@rocketmail.com>
2025-02-15 08:32:36 +0100

    removed NOT NULL constraint for catefgory_id column in expense table

--- src/database/migrations/003_create_expense_table.sql
+++ src/database/migrations/003_create_expense_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE expense (
     name VARCHAR(50) NOT NULL,
     amount DECIMAL(10, 2) NOT NULL CHECK (amount >= 0),
     description TEXT,
-    category_id UUID NOT NULL REFERENCES category(id) ON DELETE CASCADE,
+    category_id UUID REFERENCES category(id) ON DELETE SET NULL,
     user_id UUID NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
